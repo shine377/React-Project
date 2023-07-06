956c6c39bbdb8bf62689dda9b8511d091e3bf857
Shiny <shinymary.chandran@crystaldelta.com>
2023-07-06 17:49:23 +0530

    initial commit

---
+++ src/json/utils/App.js
@@ -0,0 +1,15 @@
+import Extension from "../../components/assessmentExtension";
+import Table from "../../components/tableData";
+import Form from '../../components/extensionForm'
+
+function App() {
+  return (
+    <>
+      <Extension />
+      <Table/>
+      <Form />
+    </>
+  );
+}
+
+export default App;
